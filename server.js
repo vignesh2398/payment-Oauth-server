6b1dccfc6e93ebfeefba0cbc9e8b15b0f35adbb3
vignesh2398 <rvignesh60474@gmail.com>
2022-12-06 22:11:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const cookieSession = require("cookie-session");
+const passportStrategy = require("./passport");
+const payment = require("./routes/payment");
+
+const app = express();
+
+app.use(express.json())
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["auth"],
+		maxAge: 24 * 60 * 60 * 100,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
+
+app.use("/auth", authRoute);
+app.use("/api/payment/", payment);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listenting on port ${port}...`));
